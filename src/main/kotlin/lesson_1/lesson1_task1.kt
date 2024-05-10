405c2f12fcbd9a7b554f20550e9241c669626984
initial commit <1233illia1233@gmail.com>
2024-05-10 18:09:57 +0100

    KS-1-1 shop logic

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson_1
 
 fun main() {
     val numOfOrders: Byte = 75
-    val greeting: String = "Спасибо за покупку, приходите еще!"
+    val greetingForBuying: String = "Спасибо за покупку, приходите еще!"
 }
