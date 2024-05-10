57481e4fdc098ee71a922a3d18ce641f2d2abcc8
initial commit <1233illia1233@gmail.com>
2024-05-10 19:45:14 +0100

    KS-1-4 Space

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_1
+
+fun main(){
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    val multiString = """
+        $length
+        $age
+        $partOfDay
+        $seconds
+        $partOfYear
+        $apogee
+    """.trimIndent()
+    print(multiString)
+}
