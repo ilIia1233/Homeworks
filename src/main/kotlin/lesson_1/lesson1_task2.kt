bbc65452916e3c40dd5fb67775d25d6c64ab3676
initial commit <1233illia1233@gmail.com>
2024-05-10 18:30:18 +0100

    KS-1-2 shop logic

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val numOfOrders: Byte = 75
+    val greetingForBuying: String = "Спасибо за покупку, приходите еще!"
+    var numOfWorkers: Short = 2000
+    numOfWorkers--
+
+    println(numOfOrders)
+    println(greetingForBuying)
+    //println(numOfWorkers)
+    println(numOfWorkers)
+}
