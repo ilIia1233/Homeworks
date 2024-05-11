5242565cf04936b257d684b3e21298aaa5d29285
initial commit <1233illia1233@gmail.com>
2024-05-11 17:08:32 +0100

    KS-2-4 resource with buff

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main(){
+    val silverOre = 11
+    val crystalOre = 7
+    val buffModificator = 1.2f
+
+    val modificatedSilverOre = silverOre * buffModificator
+    val modificatedCrystalOre = crystalOre * buffModificator
+
+    print("""
+        Добыто серебряной руды: ${String.format("%.0f",modificatedSilverOre)}
+        Добыто кристалической руды: ${String.format("%.0f", modificatedCrystalOre)}
+    """.trimIndent())
+}
