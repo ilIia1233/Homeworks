39395a4b0cbfc86d1b757aa59272f954662c8048
initial commit <1233illia1233@gmail.com>
2024-05-11 15:11:20 +0100

    KS-1-3 Space

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,18 +1,16 @@
 package org.example.lesson_1
 
-fun main(){
-    val year: Short = 1961
-    var hour: Byte? = null
-    var minute: Byte? = null
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
 
     println(year)
     println(hour)
     println(minute)
 
-    hour = 9
-    minute = 7
-    print("0$hour:0$minute ")
     hour = 10
     minute = 55
-    print("$hour:$minute")
+
+    println("$hour:$minute")
 }
