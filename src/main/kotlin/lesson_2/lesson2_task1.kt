92148d91415ca125bafec2be1213c9465c037158
initial commit <1233illia1233@gmail.com>
2024-05-11 14:52:59 +0100

    KS-2-1 Average mark

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main(){
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+    val amountOFMarks = 4f
+
+    val sumOfMarks = mark1 + mark2 + mark3 + mark4
+    val average = sumOfMarks / amountOFMarks
+
+    println(average)
+}
