b1624bcc057ebc3b4600d56f8d871f0f35ae78ae
initial commit <1233illia1233@gmail.com>
2024-05-13 19:39:57 +0100

    KS-2-4 Buff modificator

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,15 +3,16 @@ package org.example.lesson_2
 fun main() {
     val silverOre = 11
     val crystalOre = 7
-    val buffModificator = 1.2f
+    val buffModificator: Float
 
+    buffModificator = 1.2f
     val modificatedSilverOre = silverOre * buffModificator
     val modificatedCrystalOre = crystalOre * buffModificator
 
     print(
         """
-        Добыто серебряной руды: ${String.format("%.0f", modificatedSilverOre)}
-        Добыто кристалической руды: ${String.format("%.0f", modificatedCrystalOre)}
+        Получено дополнительно серебряной руды: ${String.format("%.0f", modificatedSilverOre - silverOre)}
+        Получено дополнительно кристалической руды: ${String.format("%.0f", modificatedCrystalOre - crystalOre)}
     """.trimIndent()
     )
 }
