3d8cb5dc0795fc46dfb0415442525c0a38599061
initial commit <1233illia1233@gmail.com>
2024-05-30 18:46:18 +0100

    KS-3-1 DayTime Greetings

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName = "Сергей"
+    var greeting: String
+
+    greeting = "Добрый день, $userName!"
+    println(greeting)
+
+    greeting = "Добрый вечер, $userName!"
+    println(greeting)
+}
